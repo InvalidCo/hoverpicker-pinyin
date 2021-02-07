cb2ff657e091eb6eafd597532682e1e7a77dab57
Mikael Kasimir Harvilahti <mikael.harvilahti@gmail.com>
2021-02-07 14:41:32 +0200

    fetchCharacterInformation returns promise

--- source.js
+++ source.js
@@ -38,46 +38,44 @@ class PinyinPicker extends ToolTippify(HoverOverCharacterPicker) {
 
 	// download html (can't do raw wiki markup as it does not have the template renderings baked in)
 	fetchCharacterInformation(c) {
-		let url = "https://en.wiktionary.org/w/index.php?title="+encodeURIComponent(c);
+		return new Promise((resolve, reject) => {
+			let url = "https://en.wiktionary.org/w/index.php?title="+encodeURIComponent(c);
 
-		// other xml fetch + parse combinations do not seem to work out of firefox extension content script
-		let request = new XMLHttpRequest();
+			// other xml fetch + parse combinations do not seem to work out of firefox extension content script
+			let request = new XMLHttpRequest();
 
-		request.open("GET", url, true);
-		request.responseType = "document";
+			request.open("GET", url, true);
+			request.responseType = "document";
 
-		request.onload = (e) => {
-			let result = "¯\\_(ツ)_/¯";
-			if(request.readyState !== 4) return;
-			if(request.status < 400)  {
-				try {
-					// parse xml, grab pinyin pronounciation and chinese definitions
-					let doc = request.responseXML;
-					let mainTitle = doc.querySelector("span.Hani").innerText;
-					let pinyinPronounciation = doc.querySelector("span.pinyin-t-form-of")?.innerText || doc.querySelector("span.pinyin-ts-form-of")?.innerText || "???";
-					let definitions;
-					let definitionTitles = doc.querySelectorAll("strong.Hani,headword[lang='zh']");
-					for (let definitionTitle of definitionTitles) {
-						if(definitionTitle.parentElement.nextElementSibling?.tagName === "OL" && definitionTitle.parentElement.previousElementSibling?.innerText === "Definitions[edit]") {
-							definitions = definitionTitle.parentElement.nextElementSibling;
-							break;
+			request.onload = (e) => {
+				let result = "¯\\_(ツ)_/¯";
+				if(request.readyState !== 4) return;
+				if(request.status < 400)  {
+					try {
+						// parse xml, grab pinyin pronounciation and chinese definitions
+						let doc = request.responseXML;
+						let mainTitle = doc.querySelector("span.Hani").innerText;
+						let pinyinPronounciation = doc.querySelector("span.pinyin-t-form-of")?.innerText || doc.querySelector("span.pinyin-ts-form-of")?.innerText || "???";
+						let definitions;
+						let definitionTitles = doc.querySelectorAll("strong.Hani,headword[lang='zh']");
+						for (let definitionTitle of definitionTitles) {
+							if(definitionTitle.parentElement.nextElementSibling?.tagName === "OL" && definitionTitle.parentElement.previousElementSibling?.innerText === "Definitions[edit]") {
+								definitions = definitionTitle.parentElement.nextElementSibling;
+								break;
+							}
 						}
+						// construct tooltip html
+						result = "<h4>" + mainTitle + " " + pinyinPronounciation + "</h4>";
+						if (definitions) result += sanitiseHtml(definitions);
+					} catch(err) {
+						console.log(err);
 					}
-					// construct tooltip html
-					result = "<h4>" + mainTitle + " " + pinyinPronounciation + "</h4>";
-					if (definitions) result += sanitiseHtml(definitions);
-				} catch(err) {
-					console.log(err);
 				}
-			}
-			this.store(c,result);
-			// still waiting on this character? if so, update tooltip
-			if(this.pendingOn === c) {
-				this.show(result);
-			}
-		};
+				resolve(result);
+			};
 
-		request.send(null);
+			request.send(null);
+		});
 	}
 
 	pick(character) {
@@ -91,7 +89,14 @@ class PinyinPicker extends ToolTippify(HoverOverCharacterPicker) {
 				r = "" + character + "...";
 				this.store(character, r);
 				this.show(r);
-				this.fetchCharacterInformation(character);
+				this.fetchCharacterInformation(character)
+				.then((result) => {
+					this.store(character, result);
+					// still waiting on this character? if so, update tooltip
+					if(this.pendingOn == character) {
+						this.show(result);
+					}
+				});
 			}
 		});
 	}
